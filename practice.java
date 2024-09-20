a061d2791b62268870d2fec42921d83f554d6eaf
Chaudhary0501 <chhayachaudhary1212@gmail.com>
2024-09-20 20:30:00 +0530

    commited

--- practice.java
+++ practice.java
@@ -3,8 +3,8 @@ public class practice{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
                 System.out.print("* ");
             }
             System.out.println("");
