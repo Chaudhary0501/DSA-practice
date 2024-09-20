4fa8bd2642af7670b12a9c653c07e1bd0c9bb5d0
Chaudhary0501 <chhayachaudhary1212@gmail.com>
2024-09-20 20:55:11 +0530

    done

--- practice.java
+++ practice.java
@@ -3,11 +3,15 @@ public class practice{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=n;i>0;i--){
-            for(int j=0;j<i;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
                 System.out.print("* ");
             }
-            System.out.println("");
+            
+            System.out.println();
         }
         
     }
