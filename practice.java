c8f358b6be458d17cb0d1657b307bc34a316a529
Chaudhary0501 <chhayachaudhary1212@gmail.com>
2024-09-20 22:40:01 +0530

    done

--- practice.java
+++ practice.java
@@ -5,9 +5,9 @@ public class practice{
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=n-i;j>=1;j--){
-                System.out.print("    ");
+                System.out.print("  ");
             }
-            for(int k=1;k<=i;k++){
+            for(int k=1;k<=2*i-1;k++){
                 System.out.print("* ");
             }
             
